749ddcb4b279c6920a3ffc00961621bbf23e716c
Stansign <60438045+StanSign@users.noreply.github.com>
2021-03-28 15:06:43 +0900

    Xylophone Done

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -19,12 +19,15 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         
-        var title = sender.currentTitle
-        playSound(title)
+        playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
     }
     
-    func playSound(title: String) {
-        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
     }
