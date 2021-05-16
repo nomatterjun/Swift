bb58fee3e578bd9b65349efee01317fe7c76fd9d
Stansign <60438045+StanSign@users.noreply.github.com>
2021-05-16 16:25:27 +0900

    Clima Update

---
+++ Closures.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+func calculator(n1: Int, n2: Int, operation: (Int, Int) -> Int) -> Int {
+    return operation(n1, n2)
+}
+
+let result = calculator(n1: 2, n2: 3) {$0 * $1}
+print(result)
+
+func add(no1: Int, no2: Int) -> Int {
+    return no1 + no2
+}
+func multiply(no1: Int, no2: Int) -> Int {
+    return no1 * no2
+}
+
+calculator(n1: 2, n2: 3) {$0 * $1}
---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by 이창준 on 2021/05/16.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
