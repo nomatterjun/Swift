9c4ec1b35c31b9948b99657db225d1dae409e7b6
Stansign <60438045+StanSign@users.noreply.github.com>
2021-05-17 16:22:40 +0900

    Flash Chat Finish

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 이창준 on 2021/05/17.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
